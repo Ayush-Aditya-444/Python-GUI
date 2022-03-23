bc518c9979ebce1d839cd15a389d65a195be863f
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-23 17:16:32 +0530

    Add files via upload

---
+++ Positioning With Tkinter's Grid System.py
@@ -0,0 +1,7 @@
+from tkinter import*
+root=Tk()
+my_label1=Label(root, text="Hello World!")
+my_label2=Label(root, text="My Name is Ayush")
+my_label1.grid(row=0, column=0)
+my_label2.grid(row=1, column=0)
+root.mainloop()
